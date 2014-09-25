30fc3f95dd9b170c45d437d562fa2d48b2a2b166
Benjamin Altpeter <benjamin@oldpeter.de>
2014-09-25 22:42:45 +0200

    Fixed error in LibraryManager

--- lib/vendor/Bookshelf/Core/LibraryManager.php
+++ lib/vendor/Bookshelf/Core/LibraryManager.php
@@ -13,8 +13,12 @@ class LibraryManager {
         $this->database_connection = new DatabaseConnection();
     }
 
-    // TODO: Expression is not allowed as parameter default value
-    function addBook($filename, $metadata = new BookMetadata()){
+    function addBook($filename, $metadata = null) {
+        // unfortunately, there is no other way to set a parameter default value to an expression: http://stackoverflow.com/a/5859401
+        if($metadata === null) {
+            $metadata = new BookMetadata();
+        }
+
         require Application::LIB_DIR . 'config.php';
 
         $file_hash = DataIo\FileManager::hash($LIBRARY_DIR . '/' . $filename);
