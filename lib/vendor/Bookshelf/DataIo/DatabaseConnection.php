24abbd34c1591b3b4bcd05177d4799607840787d
Benjamin Altpeter <benjamin@oldpeter.de>
2014-09-25 18:30:08 +0200

    Fixes #9: Change MySQL functions to OO style

--- lib/vendor/Bookshelf/DataIo/DatabaseConnection.php
+++ lib/vendor/Bookshelf/DataIo/DatabaseConnection.php
@@ -5,29 +5,29 @@ namespace Bookshelf\DataIo;
 use Bookshelf\Core\Application;
 
 class DatabaseConnection {
-    private $con;
+    private $mysqli;
 
     function __construct() {
         require Application::ROOT_DIR . 'config.php';
 
-        $this->con = mysqli_connect($DB_HOST, $DB_USER, $DB_PW, $DB_NAME);
+        $this->mysqli = new \mysqli($DB_HOST, $DB_USER, $DB_PW, $DB_NAME);
 
-        if(mysqli_connect_errno()) {
+        if($this->mysqli->connect_errno) {
             echo 'Could not connect to database.';
         }
     }
 
     // TODO: Implement proper methods for certain database actions; ideally we don't want to directly run SQL queries at all
     function execute_query($query) {
-        $result = mysqli_query($this->con, $query);
+        $result = $this->mysqli->query($query);
 
-        return mysqli_fetch_array($result);
+        return $result->fetch_array();
     }
     
     function insertLibraryData($args) {
         $columns = join(', ', array_keys($args));
         $values = join(', ', $args);
-        $sql = "INSERT INTO library ($columns) VALUES ($values)";
-        mysqli_query($this->con, $sql);
+        $query = "INSERT INTO library ($columns) VALUES ($values)";
+        $this->mysqli->query($query);
     }
 }
