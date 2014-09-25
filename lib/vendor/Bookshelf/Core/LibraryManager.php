13783b2665674dc8b2ccd84bbfb0786f1e555e19
Lorenz Sieben <lorenzsieben@googlemail.com>
2014-09-25 22:55:30 +0200

    Really Fixed Merge Conflict (You better save... ;))

--- lib/vendor/Bookshelf/Core/LibraryManager.php
+++ lib/vendor/Bookshelf/Core/LibraryManager.php
@@ -12,10 +12,6 @@ class LibraryManager {
     function __construct() {
         $this->database_connection = new DatabaseConnection();
     }
-<<<<<<< HEAD
-    
-    function addBook($file_name, $metadata = new BookMetadata()){
-=======
 
     function addBook($filename, $metadata = null) {
         // unfortunately, there is no other way to set a parameter default value to an expression: http://stackoverflow.com/a/5859401
@@ -25,7 +21,6 @@ class LibraryManager {
 
         require Application::LIB_DIR . 'config.php';
 
->>>>>>> 2517c36f500501c88b58b664d37121f0594eb826
         $file_hash = DataIo\FileManager::hash($LIBRARY_DIR . '/' . $filename);
         $args = array('file_name' => "'" . $file_name . "'",
                       'file_hash' => "'" . $file_hash . "'");
@@ -35,7 +30,6 @@ class LibraryManager {
             $args[$key] = "'" . $value . "'";
         }
         
-<<<<<<< HEAD
         $this->database_connection->insertLibraryData($args)
     }
     
@@ -48,8 +42,5 @@ class LibraryManager {
     function listBooks() {
         $result = $this->database_connection->selectLibraryData(NULL, array('file_name', 'file_hash', 'title'));
         return (empty($result[0]) ? -1 : $result[0]);
-=======
-        $this->database_connection->insertLibraryData($args);
->>>>>>> 2517c36f500501c88b58b664d37121f0594eb826
     }
 }
