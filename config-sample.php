876b74cd7b1a26d3ef0e95aa8f1996858d912276
Benjamin Altpeter <benjamin@oldpeter.de>
2014-10-05 18:04:03 +0200

    Added config.php to gitignore, added config-sample.php

---
+++ config-sample.php
@@ -0,0 +1,10 @@
+<?php
+$DB_HOST = 'localhost';
+$DB_USER = 'user';
+$DB_PW   = 'password';
+$DB_NAME = 'database-name';
+
+// you can get a salt from here https://api.wordpress.org/secret-key/1.1/salt/
+$SALT    = 'mysalt';
+
+$LIBRARY_DIR = '/var/www/bookshelf/library';
