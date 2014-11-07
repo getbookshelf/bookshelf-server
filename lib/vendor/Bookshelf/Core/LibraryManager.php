ffab8f9c69f2ddb83b93b2bddc688cf00d29eb9b
Benjamin Altpeter <benjamin@oldpeter.de>
2014-11-07 23:20:07 +0100

    Fixes #20: Added option to pass existing db connection to Configuration constructor

--- lib/vendor/Bookshelf/Core/LibraryManager.php
+++ lib/vendor/Bookshelf/Core/LibraryManager.php
@@ -9,24 +9,23 @@ use Bookshelf\DataIo;
 class LibraryManager {
     private $database_connection;
     private $config;
-    
+
     public function __construct() {
         $this->database_connection = new DatabaseConnection();
-        // TODO: We should probably allow to pass an existing DB connection to the Configuration constructor as we have one here already
-        $this->config = new Configuration();
+        $this->config = new Configuration(true, $this->database_connection);
     }
 
     // TODO: Are these methods needed in here? They just forward to the DB con methods...
     public function addBook($book) {
         return $this->database_connection->insertBook($book);
     }
-    
+
     public function getBook($file_name, $file_hash) {
         // TODO: Doesn't work anymore
         //$result = $this->database_connection->getBook(array('file_name' => $file_name, 'file_hash' => $file_hash));
         //return (empty($result[0]) ? -1 : $result[0]);
     }
-    
+
     public function listBooks() {
         // TODO: Doesn't work anymore
         //$result = $this->database_connection->getBook(null, array('file_name', 'file_hash', 'title'));
