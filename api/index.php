c45853b972a36f22b34a023ac92e225affa97b1d
Benjamin Altpeter <benjamin@oldpeter.de>
2015-03-30 00:36:05 +0200

    API can now also be accessed if there is an active session

--- api/index.php
+++ api/index.php
@@ -1,7 +1,8 @@
 <?php
 require __DIR__ . '/../lib/vendor/autoload.php';
 
-if(\Bookshelf\Utility\User::isAuthenticated($_POST['user'], $_POST['password'])) {
+session_start();
+if(\Bookshelf\Utility\User::isAuthenticated($_POST['user'], $_POST['password']) || isset($_SESSION['name'])) {
     $request = new \Bookshelf\DataIo\ApiRequest($_POST);
     $lib_man = new \Bookshelf\Core\LibraryManager();
     $file_man = new \Bookshelf\DataIo\FileManager();
