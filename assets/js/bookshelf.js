cf001f02a202bc026b10fd6f30d4f156cfffe959
Benjamin Altpeter <benjamin@oldpeter.de>
2015-03-30 02:03:34 +0200

    Implemented deleteBook function in JS

---
+++ assets/js/bookshelf.js
@@ -0,0 +1,23 @@
+function apiRequest(base_url, action, parameters, success_delegate, error_delegate) {
+    // TODO: For debugging purposes only, to be removed later
+    if(typeof success_delegate == 'undefined') { success_delegate = function(data){console.log("Success."); console.log(data);}; }
+    if(typeof error_delegate == 'undefined') { error_delegate = function(data){console.log("Error: " + data["responseJSON"]["error_code"] + " (" + data["responseJSON"]["error"] + ")");}; }
+
+    var data = { action: action };
+    $.extend(data, parameters);
+
+    $.ajax({
+        url: base_url + "/api/index.php",
+        type: "POST",
+        data: data,
+        success: success_delegate,
+        error: error_delegate
+    });
+}
+
+function deleteBook(base_url, id) {
+    if(window.confirm("Do you really want to delete this book?")) {
+        apiRequest(base_url, "deleteBook", { id: id });
+        window.location = base_url;
+    }
+}
--- book.php
+++ book.php
@@ -5,7 +5,9 @@ insertHeader();
 $id = $_GET['id'];
 if(!empty($id)) {
     $lib_man = new \Bookshelf\Core\LibraryManager();
+    $config = new \Bookshelf\Core\Configuration(true);
     $book = $lib_man->getBookById($id);
+    $base_url = $config->getBaseUrl();
 
     if($book == new \Bookshelf\DataType\Book('','','')) {
         \Bookshelf\Utility\ErrorHandler::throwError('Book not found.', \Bookshelf\Utility\ErrorLevel::ERROR);
@@ -16,7 +18,7 @@ if(!empty($id)) {
     echo '<img class="book-detail" src="' . $book->metadata->cover_image . '">';
     echo '<div id="book-info"><h1>' . $book->metadata->title . '</h1>by <span class="author">' . $book->metadata->author .'</span></div>';
     echo '<p>' . $book->metadata->description . '</p>';
-    echo '<p><a href="download.php?id=' . $id .'">Download book</a> <a href="delete_book.php?id=' . $id . '">Delete book</a></p>';
+    echo '<p><a href="download.php?id=' . $id .'">Download book</a><br><a href="#" onclick="deleteBook(\'' . $base_url . '\', ' . $id .');return false;">Delete book</a></p>';
 }
 else {
     \Bookshelf\Utility\ErrorHandler::throwError('No book selected.', \Bookshelf\Utility\ErrorLevel::ERROR);
